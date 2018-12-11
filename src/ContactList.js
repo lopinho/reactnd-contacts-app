fd8826a255bd85b38fbe2682ae1140a2b5f066b5
lopinho <lopinho@gmail.com>
2018-12-11 08:31:17 -0200

    trocando ContactList para uma funçao sem estado

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,29 +1,26 @@
 import React, {Component} from "react";
 
-class ContactList extends Component {
-    render() {
-        const {contacts} = this.props;
-        return (
-            <ol className="contact-list">
-                {contacts.map(contact => (
-                    <li key={contact.id} className="contact-list-item">
-                        <div
-                            className="contact-avatar"
-                            style={{
-                                backgroundImage: `url(${contact.avatarURL})`
-                            }}
-                        ></div>
-                        <div className="contact-details">
-                            <p>{contact.name}</p>
-                            <p>{contact.handle}</p>
-                        </div>
-                        <button className="contact-remove">Remove</button>
-                        
-                    </li>
-                ))}
-            </ol>
-        )
-    }
-}
+const contactList = (props) => {
+    return (
+        <ol className="contact-list">
+            {props.contacts.map(contact => (
+                <li key={contact.id} className="contact-list-item">
+                    <div
+                        className="contact-avatar"
+                        style={{
+                            backgroundImage: `url(${contact.avatarURL})`
+                        }}
+                    ></div>
+                    <div className="contact-details">
+                        <p>{contact.name}</p>
+                        <p>{contact.handle}</p>
+                    </div>
+                    <button className="contact-remove">Remove</button>                    
+                </li>
+            ))}
+        </ol>
+    )
+ }
 
-export default ContactList;
+
+export default contactList;
