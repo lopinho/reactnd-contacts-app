1c4449e25aa1c5599e43a9a7736b440b08e84263
lopinho <lopinho@gmail.com>
2018-12-11 08:17:42 -0200

    listando contatos na app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import ContactList from "./ContactList";
+
+const contacts = [
+  {
+    id: 'tyler',
+    name: 'Tyler McGinnis',
+    handle: '@tylermcginnis',
+    avatarURL: 'http://localhost:5001/tyler.jpg'
+  },
+  {
+    id: 'karen',
+    name: 'Karen Isgrigg',
+    handle: '@karen_isgrigg',
+    avatarURL: 'http://localhost:5001/karen.jpg'
+  },
+  {
+    id: 'richard',
+    name: 'Richard Kalehoff',
+    handle: '@richardkalehoff',
+    avatarURL: 'http://localhost:5001/richard.jpg'
+  },
+]
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <ContactList contacts={contacts} />
       </div>
     );
   }
